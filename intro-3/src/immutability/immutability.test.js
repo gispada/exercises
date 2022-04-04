ae6c9709f030b013f58b72e99f6acc3947726f13
Gianluca Spada <gianluca.spada@spindox.it>
2022-04-04 12:09:54 +0200

    fix immutability test

--- intro-3/src/immutability/immutability.test.js
+++ intro-3/src/immutability/immutability.test.js
@@ -123,29 +123,32 @@ describe('addNewUser', () => {
       }
     }
     const newUsers = addNewUser(users, newUser)
-    expect(newUsers).toEqual(
-      [
-        {
-          id: 10,
-          name: 'Clementina DuBuque',
-          username: 'Moriah.Stanton',
-          email: 'Rey.Padberg@karina.biz',
-          address: {
-            street: 'Kattie Turnpike',
-            suite: 'Suite 198',
-            city: 'Lebsackbury',
-            zipcode: '31428-2261',
-            geo: {
-              lat: '-38.2386',
-              lng: '57.2232'
-            }
-          },
-          phone: '024-648-3804',
-          website: 'ambrose.net'
+    expect(newUsers).toEqual([
+      {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        address: {
+          street: 'Kattie Turnpike',
+          suite: 'Suite 198',
+          city: 'Lebsackbury',
+          zipcode: '31428-2261',
+          geo: {
+            lat: '-38.2386',
+            lng: '57.2232'
+          }
+        },
+        phone: '024-648-3804',
+        website: 'ambrose.net',
+        company: {
+          name: 'Hoeger LLC',
+          catchPhrase: 'Centralized empowering task-force',
+          bs: 'target end-to-end models'
         }
-      ],
+      },
       newUser
-    )
+    ])
     expect(newUsers).not.toBe(users)
   })
 })
